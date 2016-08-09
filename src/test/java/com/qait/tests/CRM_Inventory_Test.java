b3bd52cf6d9f5d1d2317f2c7d23e16c90c17c6bc
unknown <deepakkumar1@ASDP1513PO01976.qait.com>
2016-08-09 13:07:18 +0530

    Updated CRM Inventory Test

--- src/test/java/com/qait/tests/CRM_Inventory_Test.java
+++ src/test/java/com/qait/tests/CRM_Inventory_Test.java
@@ -19,7 +19,6 @@ public class CRM_Inventory_Test extends BaseTest {
 	@BeforeClass
 	public void OpenBrowserWindow() {
 		test = new TestSessionInitiator(this.getClass().getSimpleName());
-		// app_url_givingDonate = getYamlValue("app_url_givingDonate");
 		app_url_IWEB = getYamlValue("app_url_IWEB");
 	}
 
@@ -42,7 +41,7 @@ public class CRM_Inventory_Test extends BaseTest {
 	}
 
 	@Test
-	public void Step03_Click_on_order_entry_button_and_verify_Centralized_Order_Entry_Page() {
+	public void Step03_Click_On_Order_Entry_Button_And_Verify_Centralized_Order_Entry_Page() {
 		test.memberShipPage.clickOnOrderEntryIcon();
 		test.memberShipPage.verifyCentralizedOrderEntryPage("Centralized Order Entry");
 	}
@@ -76,8 +75,6 @@ public class CRM_Inventory_Test extends BaseTest {
 
 	@Test
 	public void Step08_verify_invoice_is_added_in_the_invoices_open_batch_with_trancation_date_is_current_date() {
-		/*String productName=test.cmrInventoryActions.getProductName();
-		int index=test.memberShipPage.verifyProductUnderDetailsMenu(productName);*/
 		test.memberShipPage.verifyInvoiceIsAdded(customerFullNameList.get(0).trim());
 		
 	}
