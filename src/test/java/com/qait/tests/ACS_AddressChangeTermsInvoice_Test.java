a8da2b78ea54c6969f4790ee7dc9455e2d48a803
Kanika Sharma <kanikasharma@qainfotech.com>
2017-01-05 13:10:17 +0530

    changes made to address validation and address change terms invoice tests

--- src/test/java/com/qait/tests/ACS_AddressChangeTermsInvoice_Test.java
+++ src/test/java/com/qait/tests/ACS_AddressChangeTermsInvoice_Test.java
@@ -56,63 +56,63 @@ public class ACS_AddressChangeTermsInvoice_Test extends BaseTest{
 				.verifyUserIsOnHomePage("CRM | Overview | Overview and Setup");
 	}
 
-//	@Test(dependsOnMethods="Step01_Launch_Iweb_Application")
-//	public void Step02_Select_And_Run_Query_In_Query_Individual_Page() {
-//		String individualName;
-//		test.homePageIWEB.clickOnSideBarTab("Individuals");
-//		test.memberShipPage.clickOnTab("Query Individual");
-//		test.memberShipPage
-//				.selectAndRunQuery(getYamlValue("AddressChange.queryName"));
-//		individualName = test.acsAddressValidation
-//				.verifyIndividualProfilePage();
-//		test.homePageIWEB.verifyUserIsOnHomePage("CRM | Individuals | "
-//				+ individualName);
-//	}
-//
-//	@Test(dependsOnMethods="Step02_Select_And_Run_Query_In_Query_Individual_Page")
-//	public void Step03_Expand_individual_membership_tab_and_get_effective_date() {
-//		test.memberShipPage
-//				.expandDetailsMenuIfAlreadyExpanded("individual memberships");
-//		membershipDateList = test.individualsPage.getParticularMemberDetails(
-//				"individual memberships", "Active");
-//		test.memberShipPage.collapseDetailsMenu("individual memberships");
-//		test.memberShipPage
-//				.expandDetailsMenuIfAlreadyExpanded("chapter memberships");
-//		chpName = test.individualsPage.matchEffectiveAndExpiryDate(
-//				membershipDateList, "chapter memberships");
-//	}
-//
-//	@Test(dependsOnMethods="Step03_Expand_individual_membership_tab_and_get_effective_date")
-//	public void Step04_Naviagate_To_Contact_Info_Option_And_Add_New_Address() {
-//		test.individualsPage.navigateToContactInfoMenuOnHoveringMore();
-//		test.memberShipPage.expandDetailsMenuIfAlreadyExpanded("addresses");
-//		addressType = test.individualsPage.verifyAddressType("addresses");
-//		test.individualsPage.clickOnPlusSign("addresses", 2);
-//		test.individualsPage.addNewAddress(
-//				YamlReader.getYamlValue("AddressChange.organization"),
-//				YamlReader.getYamlValue("AddressChange.department"),
-//				YamlReader.getYamlValue("AddressChange.address"),
-//				YamlReader.getYamlValue("AddressChange.city"),
-//				YamlReader.getYamlValue("AddressChange.postalCode"),
-//				YamlReader.getYamlValue("AddressChange.state"), addressType);
-//		test.individualsPage.verifyAdditionOfNewAddress(addressType.get(0),
-//				"addresses");
-//	}
-//
-//	@Test(dependsOnMethods="Step04_Naviagate_To_Contact_Info_Option_And_Add_New_Address")
-//	public void Step05_Navigate_To_Membbership_Tab_And_Verify_Transferred_Chapter() {
-//		test.individualsPage.navigateToGeneralMenuOnHoveringMore("Membership");
-//		test.memberShipPage
-//				.expandDetailsMenuIfAlreadyExpanded("individual memberships");
-//		test.memberShipPage
-//				.expandDetailsMenuIfAlreadyExpanded("chapter memberships");
-//		test.individualsPage.verifyChapterStatusIsTransferred(
-//				"chapter memberships", chpName);
-//	}
-//
-//	@Test(dependsOnMethods="Step05_Navigate_To_Membbership_Tab_And_Verify_Transferred_Chapter")
-//	public void Step06_Verify_New_Chapter_Is_Added_Under_Chapter_Memberships_Tab() {
-//		test.individualsPage.verifyNewChapterIsAdded("chapter memberships");
-//	}
+	@Test(dependsOnMethods="Step01_Launch_Iweb_Application")
+	public void Step02_Select_And_Run_Query_In_Query_Individual_Page() {
+		String individualName;
+		test.homePageIWEB.clickOnSideBarTab("Individuals");
+		test.memberShipPage.clickOnTab("Query Individual");
+		test.memberShipPage
+				.selectAndRunQuery(getYamlValue("AddressChange.queryName"));
+		individualName = test.acsAddressValidation
+				.verifyIndividualProfilePage();
+		test.homePageIWEB.verifyUserIsOnHomePage("CRM | Individuals | "
+				+ individualName);
+	}
+
+	@Test(dependsOnMethods="Step02_Select_And_Run_Query_In_Query_Individual_Page")
+	public void Step03_Expand_individual_membership_tab_and_get_effective_date() {
+		test.memberShipPage
+				.expandDetailsMenuIfAlreadyExpanded("individual memberships");
+		membershipDateList = test.individualsPage.getParticularMemberDetails(
+				"individual memberships", "Active");
+		test.memberShipPage.collapseDetailsMenu("individual memberships");
+		test.memberShipPage
+				.expandDetailsMenuIfAlreadyExpanded("chapter memberships");
+		chpName = test.individualsPage.matchEffectiveAndExpiryDate(
+				membershipDateList, "chapter memberships");
+	}
+
+	@Test(dependsOnMethods="Step03_Expand_individual_membership_tab_and_get_effective_date")
+	public void Step04_Naviagate_To_Contact_Info_Option_And_Add_New_Address() {
+		test.individualsPage.navigateToContactInfoMenuOnHoveringMore();
+		test.memberShipPage.expandDetailsMenuIfAlreadyExpanded("addresses");
+		addressType = test.individualsPage.verifyAddressType("addresses");
+		test.individualsPage.clickOnPlusSign("addresses", 2);
+		test.individualsPage.addNewAddress(
+				YamlReader.getYamlValue("AddressChange.organization"),
+				YamlReader.getYamlValue("AddressChange.department"),
+				YamlReader.getYamlValue("AddressChange.address"),
+				YamlReader.getYamlValue("AddressChange.city"),
+				YamlReader.getYamlValue("AddressChange.postalCode"),
+				YamlReader.getYamlValue("AddressChange.state"), addressType);
+		test.individualsPage.verifyAdditionOfNewAddress(addressType.get(0),
+				"addresses");
+	}
+
+	@Test(dependsOnMethods="Step04_Naviagate_To_Contact_Info_Option_And_Add_New_Address")
+	public void Step05_Navigate_To_Membbership_Tab_And_Verify_Transferred_Chapter() {
+		test.individualsPage.navigateToGeneralMenuOnHoveringMore("Membership");
+		test.memberShipPage
+				.expandDetailsMenuIfAlreadyExpanded("individual memberships");
+		test.memberShipPage
+				.expandDetailsMenuIfAlreadyExpanded("chapter memberships");
+		test.individualsPage.verifyChapterStatusIsTransferred(
+				"chapter memberships", chpName);
+	}
+
+	@Test(dependsOnMethods="Step05_Navigate_To_Membbership_Tab_And_Verify_Transferred_Chapter")
+	public void Step06_Verify_New_Chapter_Is_Added_Under_Chapter_Memberships_Tab() {
+		test.individualsPage.verifyNewChapterIsAdded("chapter memberships");
+	}
 
 }
--- src/test/java/com/qait/tests/ACS_Address_Validation_Test.java
+++ src/test/java/com/qait/tests/ACS_Address_Validation_Test.java
@@ -46,30 +46,30 @@ public class ACS_Address_Validation_Test extends BaseTest {
 		test.homePageIWEB.verifyUserIsOnHomePage("CRM | Overview | Overview and Setup");
 	}
 		
-//	@Test(dependsOnMethods="Step01_Launch_Iweb_Application")
-//	public void Step02_Select_Query_In_Query_Individual_Page(){
-//		test.homePageIWEB.clickOnSideBarTab("Individuals");
-//		test.memberShipPage.clickOnTab("Query Individual");
-//		test.memberShipPage.selectAndRunQuery(getYamlValue("AddressValidation.queryName"));
-//	}
-//	
-//	@Test(dependsOnMethods="Step02_Select_Query_In_Query_Individual_Page")
-//	public void Step03_Verify_User_Naviagated_To_Individual_Profile_Page(){
-//		individualName=test.acsAddressValidation.verifyIndividualProfilePage();
-//		test.homePageIWEB.verifyUserIsOnHomePage("CRM | Individuals | "+individualName);
-//	}
-//
-//	@Test(dependsOnMethods="Step03_Verify_User_Naviagated_To_Individual_Profile_Page")
-//	public void Step04_Enter_ZipCode_And_Verify_Replacement_Of_ZipCode(){
-//		test.memberShipPage.getContactIdOfUser("Customer");
-//		expectedZipCode=test.acsAddressValidation.getZipCode();
-//        test.acsAddressValidation.clickOnEditNameAndAddressButton();
-//        test.acsAddressValidation.verifyIndividualNameAndAddressInformationPage();
-//        test.acsAddressValidation.verifyZipCode(expectedZipCode);
-//        test.acsAddressValidation.enterZipCode(expectedZipCode,"12345");
-//        test.acsAddressValidation.verifyAddressVerificationWindow();
-//        test.acsAddressValidation.verifyAddressVerificationPageZipCode(expectedZipCode);
-//        test.acsAddressValidation.verifyReplacementOfZipCode(expectedZipCode);
-//	}
+	@Test(dependsOnMethods="Step01_Launch_Iweb_Application")
+	public void Step02_Select_Query_In_Query_Individual_Page(){
+		test.homePageIWEB.clickOnSideBarTab("Individuals");
+		test.memberShipPage.clickOnTab("Query Individual");
+		test.memberShipPage.selectAndRunQuery(getYamlValue("AddressValidation.queryName"));
+	}
+	
+	@Test(dependsOnMethods="Step02_Select_Query_In_Query_Individual_Page")
+	public void Step03_Verify_User_Naviagated_To_Individual_Profile_Page(){
+		individualName=test.acsAddressValidation.verifyIndividualProfilePage();
+		test.homePageIWEB.verifyUserIsOnHomePage("CRM | Individuals | "+individualName);
+	}
+
+	@Test(dependsOnMethods="Step03_Verify_User_Naviagated_To_Individual_Profile_Page")
+	public void Step04_Enter_ZipCode_And_Verify_Replacement_Of_ZipCode(){
+		test.memberShipPage.getContactIdOfUser("Customer");
+		expectedZipCode=test.acsAddressValidation.getZipCode();
+        test.acsAddressValidation.clickOnEditNameAndAddressButton();
+        test.acsAddressValidation.verifyIndividualNameAndAddressInformationPage();
+        test.acsAddressValidation.verifyZipCode(expectedZipCode);
+        test.acsAddressValidation.enterZipCode(expectedZipCode,"12345");
+        test.acsAddressValidation.verifyAddressVerificationWindow();
+        test.acsAddressValidation.verifyAddressVerificationPageZipCode(expectedZipCode);
+        test.acsAddressValidation.verifyReplacementOfZipCode(expectedZipCode);
+	}
 
 }
