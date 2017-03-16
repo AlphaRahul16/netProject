45ed894037da172e7a2d952a5bfcf15bd7a91250
Rahul Yadav <rahulyadav@qainfotech.com>
2017-03-16 17:58:04 +0530

    Added MAPS_30 and MAPS_32

--- src/test/java/com/qait/MAPS/keywords/Submission_Page_Actions.java
+++ src/test/java/com/qait/MAPS/keywords/Submission_Page_Actions.java
@@ -1,6 +1,7 @@
 package com.qait.MAPS.keywords;
 
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 
 import com.qait.automation.getpageobjects.ASCSocietyGenericPage;
 
@@ -14,5 +15,22 @@ public class Submission_Page_Actions extends ASCSocietyGenericPage {
 		super(driver, pagename);
 
 	}
+	
+	public void clickOnCreateNewSubmissionLink(String linkName)
+	{
+		isElementDisplayed("lnk_createSubmission",linkName);
+		click(element("lnk_createSubmission",linkName));
+		logMessage("Step : "+linkName+" link is clicked under View Submission tab");
+	}
+	
+	
+	public void verifyPageHeaderForASection(String header)
+	{
+			isElementDisplayed("txt_pageHeader");
+			Assert.assertTrue(element("txt_pageHeader").getText().contains(header));
+			logMessage("ASSERT PASSED: Page header is verified as "+header);
+		}
+	}
+	
+	
 
-}
--- src/test/java/com/qait/MAPS/tests/Maps_Submission_Test.java
+++ src/test/java/com/qait/MAPS/tests/Maps_Submission_Test.java
@@ -19,19 +19,35 @@ public class Maps_Submission_Test extends BaseTest {
 	}
 	
 	@Test
-	public void MAPS_1_Launch_Application() {
+	public void Test01_MAPS_1_Launch_Application() {
 		test.launchMAPSApplication(maps_url);
 		test.maps_SSOPage.verifyUserIsOnMAPSHomePage("Meeting Abstracts Programming System");
 	}
 	
 	@Test
-	public void MAPS_3_LogIn_With_Valid_Credentials() {
+	public void Test02_MAPS_3_LogIn_With_Valid_Credentials() {
         test.maps_SSOPage.loginWithValidCredentials(YamlReader.getYamlValue("LogIn_Details.userID"), YamlReader.getYamlValue("LogIn_Details.password"));
 	}
 	
 	@Test
-	public void MAPS_6_Verify_User_Is_Navigated_To_Welcome_Page() {
-		test.maps_SSOPage.verifyUserIsOnWelcomePage("Welcome");	
+	public void Test03_MAPS_6_Verify_User_Is_Navigated_To_Welcome_Page() {
+		test.maps_SSOPage.verifyUserIsOnTabPage("Welcome");	
 	}
+	
+	@Test
+	public void Test04_MAPS_30_Click_On_Submission_In_Top_Navigation_Menu() {
+		test.maps_SSOPage.clickOnTabOnUpperNavigationBar("Submission");
+		test.maps_SSOPage.verifyUserIsOnTabPage("Submission");	
+		test.maps_submissionPage.verifyPageHeaderForASection("View Submissions");
+	}
+	
+	@Test
+	public void Test05_MAPS_32_Click_On_Create_New_Submission_Page_Lik_And_Verify_That_User_Is_Navigated_To_Program_Area_Page() {
+		test.maps_submissionPage.clickOnCreateNewSubmissionLink("Create New Submission");	
+		test.maps_submissionPage.verifyPageHeaderForASection("Program Area");
+	}
+	
+	
+	
 
 }
