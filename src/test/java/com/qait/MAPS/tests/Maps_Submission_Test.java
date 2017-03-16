1d7990cf642d7c23cff6734e31fe141a86f334ed
Kanika Sharma <kanikasharma@qainfotech.com>
2017-03-16 17:52:06 +0530

    added Maps_94 and Maps_106 test cases

--- src/test/java/com/qait/MAPS/tests/Maps_Submission_Test.java
+++ src/test/java/com/qait/MAPS/tests/Maps_Submission_Test.java
@@ -19,19 +19,32 @@ public class Maps_Submission_Test extends BaseTest {
 	}
 	
 	@Test
-	public void MAPS_1_Launch_Application() {
+	public void Test01_MAPS_1_Launch_Application() {
 		test.launchMAPSApplication(maps_url);
 		test.maps_SSOPage.verifyUserIsOnMAPSHomePage("Meeting Abstracts Programming System");
 	}
 	
 	@Test
-	public void MAPS_3_LogIn_With_Valid_Credentials() {
+	public void Test02_MAPS_3_LogIn_With_Valid_Credentials() {
         test.maps_SSOPage.loginWithValidCredentials(YamlReader.getYamlValue("LogIn_Details.userID"), YamlReader.getYamlValue("LogIn_Details.password"));
 	}
 	
 	@Test
-	public void MAPS_6_Verify_User_Is_Navigated_To_Welcome_Page() {
+	public void Test03_MAPS_6_Verify_User_Is_Navigated_To_Welcome_Page() {
 		test.maps_SSOPage.verifyUserIsOnWelcomePage("Welcome");	
+	}
+	
+	@Test
+	public void Test04_MAPS_94_Submit_Details_On_Title_Body_Page() {//verification to be added in the end
+		test.maps_submissionPage.submitTitleAndBodyDetails("Test Title","Test Abstract");
+		test.maps_submissionPage.uploadImage("test");
+		test.maps_submissionPage.clickOnSaveAndContinueButton();
+	}
+	
+	@Test
+	public void Test05_MAPS_106_Submit_Details_Select_Symposium_Page() {
+		test.maps_submissionPage.submitDetailsOnSelectSymposiumPage("1. Oral Only","Coffee & Cocoa Products","1. Consider for Sci-Mix");
+		test.maps_submissionPage.clickOnSaveAndContinueButton();
 	}
 
 }
